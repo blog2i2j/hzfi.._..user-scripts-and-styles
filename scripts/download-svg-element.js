7d7512adb1015bc6d2de81376d7e3e3b867b2388
hzfi <710921+hzfi@users.noreply.github.com>
2025-11-07 21:44:28 +0800

    Add exclusion for SVG files in user script

--- scripts/download-svg-element.js
+++ scripts/download-svg-element.js
@@ -12,6 +12,7 @@
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=your.email.here@example.com&item_name=Greasy+Fork+donation
 // @contributionAmount 5
 // @include     *://*/*
+// @exclude     *://*/*.svg
 
 // @noframes
 // @grant          unsafeWindow
@@ -74,8 +75,9 @@ const styleContent = `.hx-download-svg-el-tool{
 // const head = document.getElementsByTagName('head');
 // head?.[0].insertAdjacentHTML('beforeend', `<style type="text/css">${styleContent}</style>`);
 
-GM_addStyle(styleContent);
-
+if ( document.documentElement.tagName.toUpperCase() === 'HTML' ) {
+   GM_addStyle(styleContent);
+}
 
 console.warn('Welcome to %c \ud83d\ude48\ud83d\ude49\ud83d\ude4a\u0020\u0048\u007a\u00b2\u0020\u0053\u0063\u0072\u0069\u0070\u0074\u0020\u004c\u0069\u0062\u0072\u0061\u0072\u0079 %c v0.06 ', 'background-color:teal;color: white;border:1px solid teal;border-radius: 4px 0 0 4px;border-left-width:0;padding:1px;margin:2px 0;font-size:1.1em', 'background-color:#777;color: white;border:1px solid #777;border-radius: 0 4px 4px 0;border-right-width:0;padding:1px;margin:5px 0;');
 
